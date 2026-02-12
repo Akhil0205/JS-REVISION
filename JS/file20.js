d0ea318f6b1e6af79b88a48863b27821777fea92
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-12 11:20:24 +0530

    map and filter

---
+++ JS/file20.js
@@ -0,0 +1,61 @@
+const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    desc: "This is description",
+    price: 100,
+    category: "Laptop",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    desc: "This is description",
+    price: 120,
+    category: "Desktop",
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    desc: "This is description",
+    price: 150,
+    category: "Laptop",
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    desc: "This is description",
+    price: 100,
+    category: "Laptop",
+  },
+  {
+    id: 5,
+    name: "Product 5",
+    desc: "This is description",
+    price: 120,
+    category: "Desktop",
+  },
+  {
+    id: 6,
+    name: "Product 6",
+    desc: "This is description",
+    price: 150,
+    category: "Laptop",
+  },
+];
+// let search="Laptop"
+// const resultArray=products.map((product)=>product)
+// console.log(resultArray)
+
+// let search="Laptop"
+// const resultArray=products.filter((product)=>
+//     {
+//         return product.category===search
+//     })
+// console.log(resultArray)
+//map is used to create new array with new or modified value
+let search="Laptop"
+const resultArray=products.map((product)=>
+    {
+        return product
+    })
+console.log(resultArray)
