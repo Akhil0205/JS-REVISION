01bb89e681998937ad104a8f2d5c854a417898b7
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-13 11:15:56 +0530

    done

--- JS/part2/file27.js
+++ JS/part2/file27.js
@@ -18,6 +18,25 @@
 // let result=score> 50 && "Pass"  //we dont want the else part
 // console.log(result)    
 
-let score=10
-let result=score || 30  //if score has no val res will be 30
-console.log(result)    
+// let score=10
+// let result=score || 30  //if score has no val res will be 30
+// console.log(result)    
+
+let score = 81;
+
+switch (true) {
+  case (score <= 100 && score > 80):
+    console.log("Grade A");
+    break;
+
+  case (score <= 80 && score > 50):
+    console.log("Grade B");
+    break;
+
+  case (score <= 50 && score >= 20):
+    console.log("Grade C");
+    break;
+
+  default:
+    console.log("Failed");
+}
