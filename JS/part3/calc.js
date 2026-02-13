696d064ab8493f6d1ca5059053fb54acabee6938
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-13 16:04:31 +0530

    updated calc

---
+++ JS/part3/calc.js
@@ -0,0 +1,18 @@
+ function add(a,b)
+{
+    return a+b
+}
+
+function subtract(a,b)
+{
+    return a-b;
+}
+
+
+
+//METHOD-2
+// export default add;
+// export { subtract };
+//THEN import add, { subtract } from "./file.js";
+
+export {add,subtract}
--- JS/part3/file30.js
+++ JS/part3/file30.js
@@ -1,6 +1,9 @@
-function add(a,b)
-{
-    return a+b
-}
+import {add,subtract} from "./calc.js"
 const result=add(4,5)
 console.log(result)
+
+
+
+
+const res=subtract(10,5)
+console.log(res)
