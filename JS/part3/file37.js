fd9c41fc9e16a9f8ae57fc163d9086e46433458b
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-16 11:33:14 +0530

    updated file37

--- JS/part3/file37.js
+++ JS/part3/file37.js
@@ -8,4 +8,6 @@ let str="Hello"
 // console.log(str.startsWith("H"))
 // console.log(str.endsWith("u"))
 
-console.log(str.slice(1,3))
+console.log(str.slice(1,4))
+console.log(str.substring(1,4))
+//both works the same
