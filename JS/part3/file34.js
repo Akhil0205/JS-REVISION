6c9082223a6d7b28d58adf62a15aac39cb7c0366
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-16 10:46:05 +0530

    updated file34

---
+++ JS/part3/file34.js
@@ -0,0 +1,12 @@
+function response(user)
+{
+    console.table(user)
+}
+const user=
+{
+    name:"John",
+    email:"john@gmail.com",
+    role:"user"
+}
+response(user)
+//we are calling function and passing entire object here
