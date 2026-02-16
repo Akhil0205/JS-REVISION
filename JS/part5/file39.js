5540da1502241f98dffdb0d810546c01df4c9cd7
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-16 13:45:24 +0530

    updated

--- JS/part5/file39.js
+++ JS/part5/file39.js
@@ -1,7 +1,21 @@
-//hoisting
-console.log(name)
-var name="John"
-
+//hoisting with var
+// console.log(name)
+// var name="John"
 // var name
 // console.log(name)
 // name="John"
+
+//hoisting with let and const
+// console.log(name)
+// let name="John"
+
+//cannot access name before initialization
+//TDZ = temporal dead zone
+
+
+greet()
+//function is fully hoisted so no error
+function greet()
+{
+    console.log("Hello World")
+}
