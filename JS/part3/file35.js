e44605fdb2d1c84c63e8bf6a82f8793bfe7f785b
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-16 11:15:55 +0530

    updated file35

---
+++ JS/part3/file35.js
@@ -0,0 +1,23 @@
+//array methods
+//const names=["Surya","John","Harsh","Aryan"]
+//const names=new Array("Surya","John","Harsh","Aryan")
+// names.push("Amit")
+// names.pop()
+//names.shift("Harsh")
+//names.shift()
+//names.unshift() 
+//unshift is slower because of rearrangement\
+//console.log(names.sort())
+//console.log(names.reverse())
+//console.log(names)
+
+
+const numbers=[15,2,1,8,3]
+//console.log(numbers.sort())
+//doesnt work for numbers
+
+//console.log(numbers.sort(()=>a-b))
+//console.log(numbers.sort(()=>b-a))
+//console.log(numbers.indexOf(2))
+console.log(numbers.lastIndexOf(2))
+console.log(numbers.includes(3))
