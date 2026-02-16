d4e7f490116abed52b9633e94effea1179b00507
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-16 10:51:39 +0530

    updated file35

--- JS/part3/file34.js
+++ JS/part3/file34.js
@@ -1,6 +1,26 @@
-function response(user)
+// function response(user)
+// {
+//     //console.table(user)
+//     console.log(user.name)
+//     console.log(user.email)
+//     console.log(user.role)
+// }
+// const user=
+// {
+//     name:"John",
+//     email:"john@gmail.com",
+//     role:"user"
+// }
+// response(user)
+//we are calling function and passing entire object here
+
+//destructuring
+function response({name,email,role})
 {
-    console.table(user)
+    //console.table(user)
+    console.log(name)
+    console.log(email)
+    console.log(role)
 }
 const user=
 {
@@ -9,4 +29,3 @@ const user=
     role:"user"
 }
 response(user)
-//we are calling function and passing entire object here
