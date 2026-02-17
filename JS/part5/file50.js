d5c0f6df5c0cf2905a577672c9f3ffe31dae81cd
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-17 11:52:01 +0530

    updated

---
+++ JS/part5/file50.js
@@ -0,0 +1,82 @@
+//other promise methods
+//Promice.race,Promise.any,Promise.allSettled
+
+//🧠 1. Promise.race()
+// 📌 Meaning:
+
+// Whichever promise finishes first wins (resolve OR reject)
+
+// It does NOT wait for others.
+
+//🧠 2. Promise.any()
+// 📌 Meaning:
+
+// First successful promise wins
+// (rejections ignored unless ALL fail)
+
+//🧠 3. Promise.allSettled()
+// 📌 Meaning:
+
+// Waits for ALL promises to finish
+// Returns results of ALL (success + failure)
+
+// Never rejects.
+
+const students = [
+  { id: 1, name: "Vansh", s1: true, s2: true, s3: false },
+  { id: 2, name: "Vaishnav", s1: true, s2: true, s3: true },
+  { id: 3, name: "Nikhil", s1: false, s2: true, s3: true },
+  { id: 4, name: "Vishnu", s1: false, s2: false, s3: false },
+];
+
+function getStudentInfo(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const found = students.find((student) => student.id === id);
+      if (found) resolve("Student Info");
+      else reject("Student does not Exist");
+    }, 2000);
+  });
+}
+function getAttendanceInfoS1(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const found = students.find((student) => student.id === id && student.s1 === true);
+      if (found) resolve();
+      else reject("Student is Absent");
+    }, 1000);
+  });
+}
+function getAttendanceInfoS2(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const found = students.find((student) => student.id === id && student.s2 === true);
+      if (found) resolve();
+      else reject("Student is Absent");
+    }, 1000);
+  });
+}
+function getAttendanceInfoS3(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const found = students.find((student) => student.id === id && student.s3 === true);
+      if (found) resolve();
+      else reject("Student is Absent");
+    }, 1000);
+  });
+}
+async function main() {
+  try {
+    const studentId = 5;
+    const found=await getStudentInfo(studentId)
+    const result = await Promise.any([
+      getAttendanceInfoS1(studentId),
+      getAttendanceInfoS2(studentId),
+      getAttendanceInfoS3(studentId),
+    ]);
+    console.log("Student is present");
+  } catch (err) {
+    console.log(err);
+  }
+}
+main();
