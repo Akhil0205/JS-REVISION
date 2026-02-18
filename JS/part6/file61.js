f17ab80e9040f1ebc3b18b94bd9cffaf467d1c57
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-18 14:41:07 +0530

    updated

--- JS/part6/file61.js
+++ JS/part6/file61.js
@@ -1,7 +1,28 @@
-const emp1={
-    name:"John",
-    display:function(){
-        console.log(this.name)
-    }
-}
-emp1.display()
+// const emp1={
+//     name:"John",
+//     display:function(){
+//         console.log(this.name)
+//     }
+// }
+// const emp2={
+//     name:"Cathy",
+//     display:function(){
+//         console.log(this.name)
+//     }
+// }
+//emp1.display()
+//emp2.display()
+
+// function Employee(name){
+//     this.name=name
+//     this.display=()=>{
+//         console.log(this.name)
+//     }
+// }
+// const emp1=new Employee("John")
+// const emp2=new Employee("Jack")
+// emp1.display()
+// emp2.display()
+
+//prototype
+
