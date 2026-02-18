4d2af543e451152e67ae04767f134b131d37f7c2
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-18 15:03:12 +0530

    updated

--- JS/part6/file62.js
+++ JS/part6/file62.js
@@ -1,12 +1,11 @@
 let num=0;
-// console.log(num || 10)
+console.log(num || 10)
 //here num is false
 //returns true value => 10 
 
 
-// console.log(num ?? 10)
+console.log(num ?? 10)
 //nullish coalescing operator
 //checks for null or undefined
 //since num=0(not null) => 0
 
-console.log(num)
