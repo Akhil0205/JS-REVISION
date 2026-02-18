69b019fd2b552e4fbf192a4e0aedcea6d42bd6a5
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-18 14:50:12 +0530

    updated

--- JS/part6/file61.js
+++ JS/part6/file61.js
@@ -24,5 +24,21 @@
 // emp1.display()
 // emp2.display()
 
+
 //prototype
+
+function Employee(name){
+    this.name=name
+}
+Employee.prototype.display=function (){
+    console.log(this.name)
+}
+const emp1=new Employee("John")
+const emp2=new Employee("Jack")
+emp1.display()
+emp2.display()
+
+//earlier display was inside the employee
+//herhe display is a part of prototype
+//having access to this.name
 
