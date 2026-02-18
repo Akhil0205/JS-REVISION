a47d8781940a70bbe540cbbc6d5a95c671768df3
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-18 15:00:36 +0530

    updated

---
+++ JS/part6/file62.js
@@ -0,0 +1,12 @@
+let num=0;
+// console.log(num || 10)
+//here num is false
+//returns true value => 10 
+
+
+// console.log(num ?? 10)
+//nullish coalescing operator
+//checks for null or undefined
+//since num=0(not null) => 0
+
+console.log(num)
